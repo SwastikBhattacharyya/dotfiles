910e5dfdd42e12dd012ecf3778c6ccad12b9b6c9
Swastik Bhattacharyya <swastikunity@gmail.com>
2024-11-25 02:43:53 +0530

    feat(swww): update swww transition-step and transition-fps
    update swww script to change transition-step to 80 and transition-fps to
    30 due to performance reasons.

--- swww/.config/swww/swww.sh
+++ swww/.config/swww/swww.sh
@@ -2,4 +2,4 @@
 
 WALLPAPERS_DIR=~/.wallpapers/
 WALLPAPER=$(find "$WALLPAPERS_DIR" -type f | shuf -n 1)
-swww img "$WALLPAPER" --transition-step 5 --transition-fps 250 --transition-type random
+swww img "$WALLPAPER" --transition-step 80 --transition-fps 30 --transition-type random
